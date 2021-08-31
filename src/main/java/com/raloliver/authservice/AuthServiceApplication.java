767cb4b881f1d854300fa8b3a4e7c6e3d4f9bf74
raloliver <contato@raloliver.com.br>
2021-08-31 22:41:55 +0100

    Data in Database

--- src/main/java/com/raloliver/authservice/AuthServiceApplication.java
+++ src/main/java/com/raloliver/authservice/AuthServiceApplication.java
@@ -1,13 +1,44 @@
 package com.raloliver.authservice;
 
+import java.util.ArrayList;
+
+import com.raloliver.authservice.domain.Role;
+import com.raloliver.authservice.domain.User;
+import com.raloliver.authservice.service.UserService;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_USER"));
+
+			userService.saveUser(new User(null, "Israel Oliveira", "israel", "password", new ArrayList<>()));
+			userService.saveUser(new User(null, "Ral", "ral", "password", new ArrayList<>()));
+			userService.saveUser(new User(null, "Oliver", "oliver", "password", new ArrayList<>()));
+			userService.saveUser(new User(null, "Ral Oliver", "raloliver", "password", new ArrayList<>()));
+
+			userService.addUserRole("ral", "ROLE_USER");
+			userService.addUserRole("israel", "ROLE_USER");
+			userService.addUserRole("israel", "ROLE_MANAGER");
+			userService.addUserRole("oliver", "ROLE_ADMIN");
+			userService.addUserRole("raloliver", "ROLE_USER");
+			userService.addUserRole("raloliver", "ROLE_ADMIN");
+			userService.addUserRole("raloliver", "ROLE_SUPER_USER");
+		};
 	}
 
 }
--- src/main/java/com/raloliver/authservice/api/UserResource.java
+++ src/main/java/com/raloliver/authservice/api/UserResource.java
@@ -2,15 +2,21 @@ package com.raloliver.authservice.api;
 
 import com.raloliver.authservice.service.UserService;
 
+import java.net.URI;
 import java.util.List;
 
+import com.raloliver.authservice.domain.Role;
 import com.raloliver.authservice.domain.User;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -23,4 +29,35 @@ public class UserResource {
     public ResponseEntity<List<User>> getUser() {
         return ResponseEntity.ok().body(userService.getUsers());
     }
+
+    @PostMapping("/user")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    /**
+     * There is no response for the user whem add a role
+     * 
+     * @param form
+     * @return
+     */
+    @PostMapping("/role/add")
+    public ResponseEntity<?> addRole(@RequestBody RoleToUserForm form) {
+        userService.addUserRole(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+}
+
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
 }

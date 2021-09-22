7870c64c9afd48e30a0ffa61e3e70f67ef99cc46
raloliver <contato@raloliver.com.br>
2021-09-22 15:08:18 +0100

    Response with access_token and refresh_token

--- src/main/java/com/raloliver/authservice/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/raloliver/authservice/filter/CustomAuthenticationFilter.java
@@ -2,23 +2,32 @@
  * File: CustomAuthenticationFilter.java
  * Project: filter
  * Created: Wednesday, September 22nd 2021, 6:40:05 am
- * Last Modified: Wednesday, September 22nd 2021, 7:00:56 am
+ * Last Modified: Wednesday, September 22nd 2021, 3:07:34 pm
  * Copyright © 2021 AMDE Agência
  */
 
 package com.raloliver.authservice.filter;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.extern.slf4j.Slf4j;
@@ -46,10 +55,28 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         return authenticationManager.authenticate(authenticationToken);
     }
 
+    /**
+     * TODO: remove secret from this method and pass by some encrypt/decrypt class
+     */
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
-            Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+            Authentication authentication) throws IOException, ServletException {
+        User user = (User) authentication.getPrincipal();
+        Algorithm algorithm = Algorithm.HMAC256("fy2oP3o9".getBytes());
+
+        String access_token = JWT.create().withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                .withIssuer(request.getRequestURI().toString())
+                .withClaim("roles",
+                        user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .sign(algorithm);
+
+        String refresh_token = JWT.create().withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
+                .withIssuer(request.getRequestURI().toString()).sign(algorithm);
+
+        response.setHeader("access_token", access_token);
+        response.setHeader("refresh_token", refresh_token);
     }
 
 }
